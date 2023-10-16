18d92b9cb6b221d25125df509bca896526837cd8
Christopher Schweigart <chris.schweigart@venomdesign.com>
2023-10-15 22:28:21 -0400

    fix path for now

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { LAYOUT } from '@layout/*';
+import { LAYOUT } from './shared/layout';
 import { ButtonComponent } from './shared/components';
 import { CardComponent } from './shared/components';
 
